d4ab54bfc3d5da8b22c938d544f6a992a42af93c
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2021-08-01 00:28:27 -0500

    *Se crea dataClass de super Hero

--- app/src/main/java/com/xx1196/superherolist/SuperHero.kt
+++ app/src/main/java/com/xx1196/superherolist/SuperHero.kt
@@ -2,7 +2,7 @@ package com.xx1196.superherolist
 
 data class SuperHero(
     val name: String,
-    val realName: String,
     val publisher: String,
+    val realName: String,
     val image: String
 )
